309ad1f275f5ea107f5cc41ae93722a452f567e1
Valentin Lab <valentin.lab@kalysto.org>
2012-06-22 10:55:13 +0200

    chg: minor pylint and pep8 changes @minor

--- ir_report.py
+++ ir_report.py
@@ -28,10 +28,12 @@ class ReportXML(osv.osv):
 
     def register_all(self,cursor):
         value = super(ReportXML, self).register_all(cursor)
-        cursor.execute("SELECT * FROM ir_act_report_xml WHERE report_type = 'xml'")
+        cursor.execute(
+            "SELECT * FROM ir_act_report_xml WHERE report_type = 'xml'")
         records = cursor.dictfetchall()
         for record in records:
-            register_report(record['report_name'], record['model'], record['report_rml'])
+            register_report(record['report_name'], record['model'],
+                            record['report_rml'])
         return value
 
     def unlink(self, cursor, user, ids, context=None):
@@ -59,7 +61,7 @@ class ReportXML(osv.osv):
     def create(self, cursor, user, vals, context=None):
         "Create report and register it"
         res = super(ReportXML, self).create(cursor, user, vals, context)
-        if vals.get('report_type','') == 'xml':
+        if vals.get('report_type', '') == 'xml':
             # I really look forward to virtual functions :S
             register_report(
                         vals['report_name'],
@@ -71,7 +73,7 @@ class ReportXML(osv.osv):
     def write(self, cr, uid, ids, vals, context=None):
         "Edit report and manage it registration"
         if isinstance(ids, (int, long)):
-            ids = [ids,]
+            ids = [ids, ]
         for rep in self.browse(cr, uid, ids, context=context):
             if rep.report_type != 'xml':
                 continue
@@ -92,13 +94,29 @@ class ReportXML(osv.osv):
     _name = 'ir.actions.report.xml'
     _inherit = 'ir.actions.report.xml'
     _columns = {
-        'xml_debug' : fields.boolean('reportXML debug', help="Enable the xml report engine debugger"),
-        'xml_full_dump' : fields.boolean('Full dump', help="Enable the full dump of the current object."),
-        'xml_full_dump_deepness' : fields.integer('Full dump deepness', help="Deepness of the full dump."),
-        'xml_full_dump_additional_data' : fields.text('Additional data', help="Data to be included in the XML full dump."),
-        'xml_full_dump_unfold_outline' : fields.text('Unfold Outline', help="Python dictionnary to draw the unfold outline of the dump"),
 
-        'xml_template' : fields.text('XML Template', help="Mako XML Template."),
+        'xml_debug': fields.boolean(
+            'reportXML debug',
+            help="Enable the xml report engine debugger"),
+
+        'xml_full_dump': fields.boolean(
+            'Full dump',
+            help="Enable the full dump of the current object."),
+
+        'xml_full_dump_deepness': fields.integer(
+            'Full dump deepness',
+            help="Deepness of the full dump."),
+
+        'xml_full_dump_additional_data': fields.text(
+            'Additional data',
+            help="Data to be included in the XML full dump."),
+
+        'xml_full_dump_unfold_outline': fields.text(
+            'Unfold Outline',
+            help="Python dictionnary to draw the unfold outline of the dump"),
+
+        'xml_template': fields.text(
+            'XML Template', help="Mako XML Template."),
     }
 
     _defaults = {
